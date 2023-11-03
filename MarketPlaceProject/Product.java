02a8772d34a412ebae391d80d02e2de45dc59183
SuhaasNachannagari <143771172+SuhaasNachannagari@users.noreply.github.com>
2023-11-02 20:11:06 -0400

    Added fields to product

--- MarketPlaceProject/Product.java
+++ MarketPlaceProject/Product.java
@@ -1,3 +1,11 @@
 public class Product {
+    private String name;
+    private String storeName;
+    private String description;
+    private int quantAvaiable;
+    private double price;
 
+    public Product() {
+
+    }
 }
