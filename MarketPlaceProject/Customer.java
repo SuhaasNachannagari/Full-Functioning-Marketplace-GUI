2a1401699221726209950544ede344c52c2835cc
YizrealHu <143854047+YizrealHu@users.noreply.github.com>
2023-11-10 17:10:04 -0500

    Unfinished Customer.java

--- MarketPlaceProject/Customer.java
+++ MarketPlaceProject/Customer.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Customer {
     String customerUserName;
-    ArrayList<Product> shoppingCar;
+    ArrayList<Product> shoppingCart;
 
-    public Customer(ArrayList<Product> shoppingCar, String customerUserName) {
+    public Customer(ArrayList<Product> shoppingCart, String customerUserName) {
         this.customerUserName = customerUserName;
-        this.shoppingCar = shoppingCar;
+        this.shoppingCart = shoppingCart;
     }
 
     public String getCustomerUserName() {
@@ -18,23 +20,49 @@ public class Customer {
     }
 
     public ArrayList<Product> getShoppingCar() {
-        return shoppingCar;
+        return shoppingCart;
     }
 
     public void setShoppingCar(ArrayList<Product> shoppingCar) {
-        this.shoppingCar = shoppingCar;
+        this.shoppingCart = shoppingCar;
     }
-//not done
+
     public void addShoppingCar(Product product) {
-        shoppingCar.add(product);
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter the product you want to buy:");
+        String input = scan.nextLine();
+
+        
+        shoppingCart.add(product);
         //multiple
     }
-//not done
+
     public void deleteShoppingCar(Product product) {
-        shoppingCar.remove(product);
+        shoppingCart.remove(product);
         //multiple
     }
-//not done
+
+    public void deleteMultipleItemsShoppingCar() {
+        //Get user input for products to remove
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter products you want to remove(use comma to separate");
+        String input = scanner.nextLine();
+        String[] productsToRemove = input.split(",");
+
+        //Use Iterator to remove products specified by the user
+        Iterator<Product> iterator = shoppingCart.iterator();
+        while (iterator.hasNext()) {
+            Product items = iterator.next();
+            for (String products : productsToRemove) {
+                if (items.equals(products.trim())) {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+    }
+
     public void buyShoppingCar () {
         // export arraylist into txt
         //clean the arraylist
