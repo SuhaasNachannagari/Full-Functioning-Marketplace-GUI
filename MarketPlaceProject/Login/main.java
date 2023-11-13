c68e6f5fa51173fdf7431e60bcae50ae0ddfcf2b
Tri Vo <voquangtri2021@gmail.com>
2023-11-13 02:52:08 -0500

    Update main.java | change back Main class

--- MarketPlaceProject/Login/main.java
+++ MarketPlaceProject/Login/main.java
@@ -1,17 +1,35 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class main extends Marketplace {
-    private static ArrayList<Seller> sellers;
+public class main {
+    public static ArrayList<Seller> sellers;
+    // added static, but not updated
+    public static ArrayList<Seller> getSellers() {
+        return sellers;
+    }
+    public static ArrayList<Customer> customers;
+    // added static for getCust, but not updated;
+    public static ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+    public static void setCustomers (ArrayList<Customer> customers) { this.customers = customers; }
 
     public static void main(String[] args) {
+
+        // this read the file and set the "sellers" variable
+        //sellers = readDataSeller();
+        sellers = readDataSeller();
+        
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Filler");//Fill in the welcome message.
         boolean correctInput = true;
         // Tri: I bring this out so that I can use this variable later;
         int custOrSell;
         //
-        String username = "";
+        String username = null;
+
         do {
             System.out.println("Are you a customer or a seller?" +
                     "(1 - Seller, 2 - Customer, 3 - Exit)");
@@ -22,8 +40,7 @@ public class main extends Marketplace {
                 boolean correctInput1 = true;
                 do {
                     System.out.println("Does the account already exist? (1 - Exists, 2 - New Account)");
-                    int input = scanner.nextInt();
-                    scanner.nextLine();
+                    int input = scanner.nextInt(); scanner.nextLine();
                     if (input == 1) {
                         boolean usernameExists;
                         boolean passwordIsCorrect;
@@ -69,6 +86,8 @@ public class main extends Marketplace {
                                 System.out.println("This username already exists.");
                             }
                         } while (usernameExists);
+                        // Add new seller
+                        sellers.add(new Seller(null, username));
                         if (username.equals("back")) {
                             break;
                         }
@@ -122,6 +141,7 @@ public class main extends Marketplace {
                         Continue The code here for an existing customer.
                          */
 
+
                     } else if (input == 2) {
                         boolean usernameExists;
                         do {
@@ -146,6 +166,7 @@ public class main extends Marketplace {
                         Rest of the code for a new account.
                          */
 
+
                     } else {
                         System.out.println("Incorrect input, please try again.");
                         correctInput1 = false;
@@ -160,7 +181,11 @@ public class main extends Marketplace {
         } while (!correctInput);
 
         //body
-        if (custOrSell == 1) {
+        boolean checkIndexUser ;
+        boolean checkIndexDoAgain;
+        int checkDoAgain;
+
+        if ( custOrSell == 1) {
             do {
                 do {
                     checkIndexUser = true;
@@ -171,104 +196,37 @@ public class main extends Marketplace {
 
                     switch (option) {
                         case 1:
-                            Delete delete = null;
-                            delete.setSeller(dummyUserName);
+                            Delete delete = new Delete();
+                            delete.setSeller(username);
+                            break;
                         case 2:
-                            Edit edit = null;
-                            edit.setSeller(dummyUserName);
+                            Edit edit = new Edit();
+                            edit.setSeller(username);
+                            break;
                         case 3:
-                            Create create = null;
-                            create.setSeller(dummyUserName);
+                            Create create = new Create();
+                            create.setSeller(username);
                             break;
-                        //Raghav
-                        case 4:
-                            //view
-                            for (int i = 0; i < sellers.size(); i++) {
-                                if (sellers.get(i).getUsername().equals(username)) {
-                                    sellers.get(i).view();
-                                }
-                            }
-                            break;
-                        case 5:
-                            //import export files
-                            int choice;
-                            boolean flag = true;
-                            do {
-                                System.out.println("Do you want to import or export files (1 - Import, 2 - Export, " +
-                                        "3 - exit");
-                                choice = scanner.nextInt();
-
-                                if (choice == 1) {
-                                    for (int i = 0; i < sellers.size(); i++) {
-                                        if (sellers.get(i).getUsername().equals(username)) {
-                                            sellers.get(i).loadFromFileProduct();
-                                        }
-                                    }
-
-                                } else if (choice == 2) {
-                                    System.out.println("Enter product details you want to add to export file:");
-                                    String productName = scanner.nextLine();
-                                    String storeName = scanner.nextLine();
-                                    String description = scanner.nextLine();
-                                    int quantity = scanner.nextInt();
-                                    double price = scanner.nextDouble();
-
-                                    Product product = new Product(productName, storeName, description, quantity, price);
-
-                                    for (int i = 0; i < sellers.size(); i++) {
-                                        if (sellers.get(i).getUsername().equals(username)) {
-                                            if (sellers.get(i).getStores().get(i).getName().equals(storeName)) {
-                                                sellers.get(i).saveToFileProduct(product);
-                                            }
-                                        } else {
-                                            System.out.println("No store found. Making a new one.");
-                                            ArrayList<Product> productsInStore = new ArrayList<>();
-                                            productsInStore.add(product);
-                                            Store store = new Store(productsInStore, storeName);
-                                            sellers.get(i).stores.add(store);
-                                        }
-                                    }
-                                } else if(choice == 3){
-                                    flag = false;
-                                } else {
-                                    System.out.println("Invalid choice. Try again");
-                                }
-                            } while (flag);
-                            break;
-
-                        case 6:
-                            //Dashboard
-                            for(int i = 0; i < sellers.size(); i++) {
-                                System.out.println("Store:");
-                                System.out.println(sellers.get(i).getStores().get(i));
-                                System.out.println("Products\tPrice");
-                                for(int j = 0; j < sellers.get(i).getStores().get(i).getProducts().size(); j++) {
-                                    System.out.println(sellers.get(i).getStores().get(i).getProducts().get(j) + "\t"
-                                            + sellers.get(i).getStores().get(i).getProducts().get(j).getSales());
-                                }
-                            }
-                            break;
-                            //Raghav
-
                         default:
                             System.out.println("Please enter the correct number!");
                             checkIndexUser = false;
+                            break;
                     }
                 } while (!checkIndexUser);
 
                 do {
-                    checkIndexDoAgain = true;
+                    checkIndexDoAgain = false;
                     System.out.println("Do you want to use the program again? ( 1 - Yes, 2 - No");
                     checkDoAgain = scanner.nextInt();
                     scanner.nextLine();
 
-                    if (checkDoAgain != 2 || checkDoAgain != 1) {
-                        checkIndexDoAgain = false;
+                    if (checkDoAgain != 2 && checkDoAgain != 1) {
+                        checkIndexDoAgain = true;
                         System.out.println("Please enter the correct number");
                     }
-                } while (!checkIndexDoAgain);
+                } while (checkIndexDoAgain);
 
-            } while (checkDoAgain == 1);
+            } while ( checkDoAgain == 1);
         }
 
         if (custOrSell == 2) {
@@ -279,7 +237,7 @@ public class main extends Marketplace {
                     System.out.println("1 - Sort, 2 - View, 3 - Search, 4 - Shopping Carts, 5 - Purchased Items, 6 - Dashboard ");
                     int option = scanner.nextInt();
                     scanner.nextLine();
-
+                    
                     switch (option) {
                         // Thomas, Suhaas, and Rohan
                         default:
@@ -289,24 +247,56 @@ public class main extends Marketplace {
                 } while (!checkIndexUser);
 
                 do {
-                    checkIndexDoAgain = true;
-                    System.out.println("Do you want to use the program again? ( 1 - Yes, 2 - No");
+                    checkIndexDoAgain = false;
+                    System.out.println("Do you want to use the program again? ( 1 - Yes, 2 - No)");
                     checkDoAgain = scanner.nextInt();
                     scanner.nextLine();
 
-                    if (checkDoAgain != 2 || checkDoAgain != 1) {
-                        checkIndexDoAgain = false;
+                    if (checkDoAgain != 2 && checkDoAgain != 1) {
+                        checkIndexDoAgain = true;
                         System.out.println("Please enter the correct number");
                     }
-                } while (!checkIndexDoAgain);
+                } while (checkIndexDoAgain);
 
-            } while (checkDoAgain == 1);
+            } while ( checkDoAgain == 1);
 
             if (checkDoAgain == 2) {
                 System.out.println("Have a good day");
             }
 
         }
+        // added but haven't updated
+        writeDateSeller();
+    }
 
+    // write information of each Seller in the Sellers ArrayList to a file called SellerInfo.bi using Object Output Stream
+    // purpose: store sellers' data (which also include Products and Stores data
+    public static void writeDateSeller() {
+        ArrayList<Seller> sellerData = getSellers();
+        try (FileOutputStream fos = new FileOutputStream("SellerInfo.bin");
+             ObjectOutput oos = new ObjectOutputStream(fos);) {
+            for (int i = 0; i < sellerData.size(); i++) {
+                oos.writeObject(sellerData.get(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // read the file (SellerInfo.bi) that contains Sellers' data, this method will return an ArrayList<Seller>
+    public static ArrayList<Seller> readDataSeller() {
+        ArrayList<Seller> result = null;
+        try (FileInputStream fis = new FileInputStream("SellerInfo.bin");
+             ObjectInputStream ois = new ObjectInputStream(fis);) {
+            result = new ArrayList<>();
+            for (; ; ) {
+                result.add((Seller) ois.readObject());
+            }
+        } catch (EOFException eof) {
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
