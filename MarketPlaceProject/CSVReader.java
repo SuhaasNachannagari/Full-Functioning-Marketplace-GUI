17a993b63daee28245d78ac111da0aad6a0820e3
YizrealHu <143854047+YizrealHu@users.noreply.github.com>
2023-11-13 17:38:02 -0500

    Update CSVReader.java

--- MarketPlaceProject/CSVReader.java
+++ MarketPlaceProject/CSVReader.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+*project 4 CSVReader
+*
+*this class reades CSV file and also prints it
+*/
 public class CSVReader {
     public static void readCSV(String s) {
         try (BufferedReader br = new BufferedReader(new FileReader(s))) {
