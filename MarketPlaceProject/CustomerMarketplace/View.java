474b77af546712ba68d3901c64f678b81d0d434a
SuhaasNachannagari <143771172+SuhaasNachannagari@users.noreply.github.com>
2023-11-13 16:09:18 -0500

    Formatted View.java

--- MarketPlaceProject/CustomerMarketplace/View.java
+++ MarketPlaceProject/CustomerMarketplace/View.java
@@ -3,6 +3,14 @@ import java.awt.image.AreaAveragingScaleFilter;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+/**
+ * A Class that extends main in order to display and enact the code that runs whenever
+ * the customer chooses to "view" in the main method
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2023</p>
+ *
+ * @version November 13, 2023
+ */
 
 
 public class View extends main {
@@ -25,27 +33,18 @@ public class View extends main {
         System.out.println("Here are your available items: ");
         int i = 1;
         for (Product product : listedProducts) {
-            System.out.printf("%d. Store: %s, Name: %s, Price: %.2f\n",
-                    i,
-                    product.getStoreName(),
-                    product.getName(),
-                    product.getPrice());
+            System.out.printf("%d. Store: %s, Name: %s, Price: %.2f\n", i, product.getStoreName(), product.getName(), product.getPrice());
             i++;
         }
     }
+
     public boolean showProduct(int num) {
         Scanner scanner = new Scanner(System.in);
         if (num < 1 || num > listedProducts.size()) {
             return false;
         }
         Product product = listedProducts.get(num - 1);
-        System.out.printf("Name: %s\n" + "  Store Name: %s\n" + "  Price: %.2f\n" +
-                        "  Quantity Available: %d\n" + "  Description: %s\n",
-                product.getName(),
-                product.getStoreName(),
-                product.getPrice(),
-                product.getQuantAvailable(),
-                product.getDescription());
+        System.out.printf("Name: %s\n" + "  Store Name: %s\n" + "  Price: %.2f\n" + "  Quantity Available: %d\n" + "  Description: %s\n", product.getName(), product.getStoreName(), product.getPrice(), product.getQuantAvailable(), product.getDescription());
         System.out.println("Would you like to see the reviews? 1 - Yes,  2 - No");
         int reviewInput = scanner.nextInt();
         if (reviewInput == 1) {
@@ -66,18 +65,12 @@ public class View extends main {
         for (Customer customer : listedCustomers) {
             if (customer.getCustomerUserName().equals(username)) {
                 ArrayList<Product> updatedShoppingCart = customer.getShoppingCar();
-                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())){
-                    System.out.println("You are attempting to add more than the limit of "
-                            + productFromSeller.getLimit() + " units set by the seller");
+                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())) {
+                    System.out.println("You are attempting to add more than the limit of " + productFromSeller.getLimit() + " units set by the seller");
                 } else if (quantity > productFromSeller.getQuantAvailable()) {
-                    System.out.println("There is only " + productFromSeller.getQuantAvailable()
-                            + " units left, you are attempting to add above that limit");
+                    System.out.println("There is only " + productFromSeller.getQuantAvailable() + " units left, you are attempting to add above that limit");
                 } else {
-                    Product productToAdd = new Product(productFromSeller.getName(),
-                            productFromSeller.getStoreName(),
-                            productFromSeller.getDescription(),
-                            quantity,
-                            productFromSeller.getPrice());
+                    Product productToAdd = new Product(productFromSeller.getName(), productFromSeller.getStoreName(), productFromSeller.getDescription(), quantity, productFromSeller.getPrice());
                     updatedShoppingCart.add(productToAdd);
                     customer.setShoppingCar(updatedShoppingCart);
                 }
@@ -85,26 +78,21 @@ public class View extends main {
             }
         }
     }
+
     public void purchaseItem(String username, int quantity, int num) {
         Product productFromSeller = listedProducts.get(num - 1);
         for (Customer customer : customers) {
             if (customer.getCustomerUserName().equals(username)) {
                 ArrayList<Product> updatedPurchaseHistory = customer.getPurchaseHistory();
-                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())){
-                    System.out.println("You are attempting to buy more than the limit of "
-                            + productFromSeller.getLimit() + " units set by the seller");
+                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())) {
+                    System.out.println("You are attempting to buy more than the limit of " + productFromSeller.getLimit() + " units set by the seller");
                 } else if (quantity > productFromSeller.getQuantAvailable()) {
-                    System.out.println("There is only " + productFromSeller.getQuantAvailable()
-                            + " units left, you are attempting to buy above that limit");
+                    System.out.println("There is only " + productFromSeller.getQuantAvailable() + " units left, you are attempting to buy above that limit");
                 } else {
-                    Product productToBuy = new Product(productFromSeller.getName(),
-                            productFromSeller.getStoreName(),
-                            productFromSeller.getDescription(),
-                            quantity,
-                            productFromSeller.getPrice());
+                    Product productToBuy = new Product(productFromSeller.getName(), productFromSeller.getStoreName(), productFromSeller.getDescription(), quantity, productFromSeller.getPrice());
                     updatedPurchaseHistory.add(productToBuy);
                     customer.setPurchaseHistory(updatedPurchaseHistory);
-                    productFromSeller.setQuantAvailable(productFromSeller.getQuantAvailable()-quantity);
+                    productFromSeller.setQuantAvailable(productFromSeller.getQuantAvailable() - quantity);
                     Store storeToUpdate = null;
                     for (Seller seller : sellers) {
                         ArrayList<Store> stores = seller.getStores();
@@ -113,20 +101,21 @@ public class View extends main {
                             for (Product product : products) {
                                 if (product.getName().equals(productToBuy.getName())) {
                                     storeToUpdate = store;
-                                    storeToUpdate.editProduct(product.getName(), 4, (""+ (productFromSeller.getQuantAvailable()-quantity)));
+                                    storeToUpdate.editProduct(product.getName(), 4, ("" + (productFromSeller.getQuantAvailable() - quantity)));
                                     store = storeToUpdate;
                                 }
                             }
                         }
                         seller.setStores(stores);
                     }
-                    if (productFromSeller.getQuantAvailable()-quantity == 0) {
+                    if (productFromSeller.getQuantAvailable() - quantity == 0) {
                         System.out.println("You have bought the entire stock");
                     }
                 }
             }
         }
     }
+
     public void addReview(String review, int num) {
         Product productToReview = listedProducts.get(num - 1);
         ArrayList<String> updatedReviews = productToReview.getReviews();
@@ -134,8 +123,6 @@ public class View extends main {
         productToReview.setReviews(updatedReviews);
 
     }
-
-
 
 
 }
