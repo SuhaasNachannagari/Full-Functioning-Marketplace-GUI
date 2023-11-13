b90c7a1ab9e410c9097957539a21f31701c08807
SuhaasNachannagari <143771172+SuhaasNachannagari@users.noreply.github.com>
2023-11-13 16:08:24 -0500

    Formatted Search.java

--- MarketPlaceProject/CustomerMarketplace/Search.java
+++ MarketPlaceProject/CustomerMarketplace/Search.java
@@ -1,7 +1,13 @@
-
 import java.util.*;
 import java.lang.*;
-
+/**
+ * A Class that extends main in order to display and enact the code that runs whenever 
+ * the customer chooses to "search" in the main method 
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2023</p>
+ *
+ * @version November 13, 2023
+ */
 
 public class Search extends main {
     private ArrayList<Seller> listedSellers = getSellers();
@@ -23,9 +29,7 @@ public class Search extends main {
         }
 
         for (Product product : listedProducts) {
-            if (product.getName().contains(search)
-            || product.getDescription().contains(search)
-            || product.getStoreName().contains(search)) {
+            if (product.getName().contains(search) || product.getDescription().contains(search) || product.getStoreName().contains(search)) {
                 matchingProducts.add(product);
             }
         }
@@ -37,16 +41,12 @@ public class Search extends main {
             System.out.println("Here are the products that match your search:");
             int i = 1;
             for (Product product : matchingProducts) {
-                System.out.printf("%d. Store: %s, Name: %s, Price:%.2f\n",
-                        i,
-                        product.getStoreName(),
-                        product.getName(),
-                        product.getPrice());
+                System.out.printf("%d. Store: %s, Name: %s, Price:%.2f\n", i, product.getStoreName(), product.getName(), product.getPrice());
                 i++;
-                }
             }
-        return true;
         }
+        return true;
+    }
 
     public boolean showProduct(int num) {
         Scanner scanner = new Scanner(System.in);
@@ -54,13 +54,7 @@ public class Search extends main {
             return false;
         }
         Product product = listedProducts.get(num - 1);
-        System.out.printf("Name: %s\n" + "  Store Name: %s\n" + "  Price: %.2f\n" +
-                        "  Quantity Available: %d\n" + "  Description: %s\n",
-                product.getName(),
-                product.getStoreName(),
-                product.getPrice(),
-                product.getQuantAvailable(),
-                product.getDescription());
+        System.out.printf("Name: %s\n" + "  Store Name: %s\n" + "  Price: %.2f\n" + "  Quantity Available: %d\n" + "  Description: %s\n", product.getName(), product.getStoreName(), product.getPrice(), product.getQuantAvailable(), product.getDescription());
         System.out.println("Would you like to see the reviews? 1 - Yes,  2 - No");
         int reviewInput = scanner.nextInt();
         if (reviewInput == 1) {
@@ -81,18 +75,12 @@ public class Search extends main {
         for (Customer customer : customers) {
             if (customer.getCustomerUserName().equals(username)) {
                 ArrayList<Product> updatedShoppingCart = customer.getShoppingCar();
-                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())){
-                    System.out.println("You are attempting to add more than the limit of "
-                            + productFromSeller.getLimit() + " units set by the seller");
+                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())) {
+                    System.out.println("You are attempting to add more than the limit of " + productFromSeller.getLimit() + " units set by the seller");
                 } else if (quantity > productFromSeller.getQuantAvailable()) {
-                    System.out.println("There is only " + productFromSeller.getQuantAvailable()
-                            + " units left, you are attempting to add above that limit");
+                    System.out.println("There is only " + productFromSeller.getQuantAvailable() + " units left, you are attempting to add above that limit");
                 } else {
-                    Product productToAdd = new Product(productFromSeller.getName(),
-                            productFromSeller.getStoreName(),
-                            productFromSeller.getDescription(),
-                            quantity,
-                            productFromSeller.getPrice());
+                    Product productToAdd = new Product(productFromSeller.getName(), productFromSeller.getStoreName(), productFromSeller.getDescription(), quantity, productFromSeller.getPrice());
                     updatedShoppingCart.add(productToAdd);
                     customer.setShoppingCar(updatedShoppingCart);
                 }
@@ -106,21 +94,15 @@ public class Search extends main {
         for (Customer customer : customers) {
             if (customer.getCustomerUserName().equals(username)) {
                 ArrayList<Product> updatedPurchaseHistory = customer.getPurchaseHistory();
-                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())){
-                    System.out.println("You are attempting to buy more than the limit of "
-                            + productFromSeller.getLimit() + " units set by the seller");
+                if ((productFromSeller.getLimit() != -1) && (quantity > productFromSeller.getLimit())) {
+                    System.out.println("You are attempting to buy more than the limit of " + productFromSeller.getLimit() + " units set by the seller");
                 } else if (quantity > productFromSeller.getQuantAvailable()) {
-                    System.out.println("There is only " + productFromSeller.getQuantAvailable()
-                            + " units left, you are attempting to buy above that limit");
+                    System.out.println("There is only " + productFromSeller.getQuantAvailable() + " units left, you are attempting to buy above that limit");
                 } else {
-                    Product productToBuy = new Product(productFromSeller.getName(),
-                            productFromSeller.getStoreName(),
-                            productFromSeller.getDescription(),
-                            quantity,
-                            productFromSeller.getPrice());
+                    Product productToBuy = new Product(productFromSeller.getName(), productFromSeller.getStoreName(), productFromSeller.getDescription(), quantity, productFromSeller.getPrice());
                     updatedPurchaseHistory.add(productToBuy);
                     customer.setPurchaseHistory(updatedPurchaseHistory);
-                    productFromSeller.setQuantAvailable(productFromSeller.getQuantAvailable()-quantity);
+                    productFromSeller.setQuantAvailable(productFromSeller.getQuantAvailable() - quantity);
                     Store storeToUpdate = null;
                     for (Seller seller : sellers) {
                         ArrayList<Store> stores = seller.getStores();
@@ -129,20 +111,21 @@ public class Search extends main {
                             for (Product product : products) {
                                 if (product.getName().equals(productToBuy.getName())) {
                                     storeToUpdate = store;
-                                    storeToUpdate.editProduct(product.getName(), 4, (""+ (productFromSeller.getQuantAvailable()-quantity)));
+                                    storeToUpdate.editProduct(product.getName(), 4, ("" + (productFromSeller.getQuantAvailable() - quantity)));
                                     store = storeToUpdate;
                                 }
                             }
                         }
                         seller.setStores(stores);
                     }
-                    if (productFromSeller.getQuantAvailable()-quantity == 0) {
+                    if (productFromSeller.getQuantAvailable() - quantity == 0) {
                         System.out.println("You have bought the entire stock");
                     }
                 }
             }
         }
     }
+
     public void addReview(String review, int num) {
         Product productToReview = listedProducts.get(num - 1);
         ArrayList<String> updatedReviews = productToReview.getReviews();
