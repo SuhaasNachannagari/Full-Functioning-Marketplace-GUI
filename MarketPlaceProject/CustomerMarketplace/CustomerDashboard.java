916f5529427bd976567869110445d87eb522aba9
RohMalpani <143854535+RohMalpani@users.noreply.github.com>
2023-11-13 16:18:39 -0500

    Update CustomerDashboard.java
    JavaDoc Update

--- MarketPlaceProject/CustomerMarketplace/CustomerDashboard.java
+++ MarketPlaceProject/CustomerMarketplace/CustomerDashboard.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * Project 4 - CustomerDashboard
+ *
+ * A Class that extends main in order to display and enact the code that runs whenever 
+ * the customer chooses to view a dashboard.
+ * It allows the customer to view all the stores and prints out the number of sales for each of them.
+ * It also displays the products the customer has bought and the stores they have bought it from.
+ *
+ */
+
 public class CustomerDashboard extends main {
     ArrayList<String> storeNames1;
     ArrayList<String> storeNames2;
