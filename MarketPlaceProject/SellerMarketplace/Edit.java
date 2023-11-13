41b675e2375683db46aaa6233bbfc48de78db0df
Tri Vo <voquangtri2021@gmail.com>
2023-11-13 02:03:33 -0500

    Update Edit.java | modify the real Sellers

--- MarketPlaceProject/SellerMarketplace/Edit.java
+++ MarketPlaceProject/SellerMarketplace/Edit.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Edit extends Marketplace {
-    private ArrayList<Seller> sellers = getSeller();
+public class Edit extends main {
     private Seller seller;
+    private int sellerIndex;
+    private int storeIndex;
+    private int indexProduct;
     public void setSeller(String userName) {
         for (int i = 0; i < sellers.size(); i++) {
             Seller sellerTemp = sellers.get(i);
             if (sellerTemp.getUserName().equals(userName)) {
                 this.seller = sellers.get(i);
+                sellerIndex = i;
             }
             break;
         }
@@ -16,7 +19,9 @@ public class Edit extends Marketplace {
     }
     public void showStore() {
         Scanner scan = new Scanner(System.in);
-        int index;
+
+        int index = 0;
+        boolean checkFormat;
         ArrayList<Store> stores = seller.getStores();
         do {
             for (int i = 0; i < stores.size(); i++) {
@@ -24,37 +29,54 @@ public class Edit extends Marketplace {
                 int j = i + 1;
                 System.out.print( j + "." + store.getName() + "\t");
             }
-            System.out.println("Enter the index of the store you want to edit: ");
-            index = scan.nextInt();
-            scan.nextLine();
+            do {
+                try {
+                    checkFormat = true;
+                    System.out.println("Enter the index of the store you want to edit: ");
+                    index = scan.nextInt();
+                    scan.nextLine();
+                } catch (NumberFormatException e) {
+                    checkFormat = false;
+                    System.out.println("Please enter the right format!");
+                }
+            } while (!checkFormat);
         } while (index > stores.size() || index <= 0); // correct index is from 1 to store.size()
 
         showAndEditProduct(index);
     }
     public void showAndEditProduct(int indexTemp) {
-        int index = indexTemp - 1;
+        storeIndex = indexTemp - 1;
         Scanner scan = new Scanner(System.in);
-        Store store = seller.getStores().get(index);
+        Store store = seller.getStores().get(storeIndex);
 
-        int indexProductTemp;
+        int indexProductTemp = 0;
+        boolean checkFormat1;
         do {
-            String[] nameCheck = new String[store.getProducts().size()]; //
+            String[] nameCheck = new String[store.getProducts().size()]; //?????
             for (int i = 0; i < store.getProducts().size(); i++) {
                 Product product = store.getProducts().get(i);
                 int j = i + 1;
                 System.out.print( j + ". " + product.getName() + "\t\t");
                 nameCheck[i] = product.getName();
             }
-            System.out.println("Enter the index of the store you want to edit: ");
-            indexProductTemp = scan.nextInt();
-            scan.nextLine();
+            do {
+                checkFormat1 = true;
+                try {
+                    System.out.println("Enter the index of the store you want to edit: ");
+                    indexProductTemp = scan.nextInt();
+                    scan.nextLine();
+                } catch (NumberFormatException e) {
+                    checkFormat1 = false;
+                    System.out.println("Please enter the right format!");
+                }
+            } while (!checkFormat1);
         } while (indexProductTemp <= 0 || indexProductTemp > store.getProducts().size());
 
-        int indexProduct = indexProductTemp - 1;
-        boolean checkFormat;
+        indexProduct = indexProductTemp - 1;
+        boolean checkFormat2;
         boolean checkOption;
         do {
-            checkFormat = true;
+            checkFormat2 = true;
             checkOption = true;
             try {
                 String name = store.getProducts().get(indexProduct).getName();
@@ -64,12 +86,12 @@ public class Edit extends Marketplace {
                 scan.nextLine();
                 System.out.println("Enter the value you want to change: ");
                 String valueChange = scan.nextLine();
-                checkOption = store.editProduct(name, indexChange, valueChange);
+                checkOption = sellers.get(sellerIndex).getStores().get(storeIndex).editProduct(name, indexChange, valueChange);
             } catch (NumberFormatException e) {
                 System.out.println("Please enter the right format!");
-                checkFormat = false;
+                checkFormat2 = false;
             }
-        } while (!checkFormat || !checkOption);
+        } while (!checkFormat2 || !checkOption);
     }
 
 }
