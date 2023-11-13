bc307ae64acfed203bfba26bca520f226b9ad63e
Tri Vo <voquangtri2021@gmail.com>
2023-11-13 05:45:23 -0500

    Update Customer.java | for displaying Dashboard for Sellers

--- MarketPlaceProject/Customer.java
+++ MarketPlaceProject/Customer.java
@@ -6,6 +6,17 @@ public class Customer {
     ArrayList<Product> shoppingCart;
     
     ArrayList<Product> purchaseHistory;
+    
+    // Tri Nov 13
+    int purchaseNumber;
+    public int getPurchaseNumber() {
+        return purchaseNumber;
+    }
+
+    public void setPurchaseNumber(int purchaseNumber) {
+        this.purchaseNumber = purchaseNumber;
+    }
+    //
 
     public Customer(ArrayList<Product> shoppingCart, String customerUserName) {
         this.customerUserName = customerUserName;
