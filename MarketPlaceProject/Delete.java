2bd1548faa48d35843ace69c5cb375d68d790a78
Tri Vo <voquangtri2021@gmail.com>
2023-11-21 16:37:49 -0500

    Update Delete.java | add warning messages and add features
    features: take into account if situation when the seller doesn't have any stores and when store doesn't have any products

--- MarketPlaceProject/Delete.java
+++ MarketPlaceProject/Delete.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -22,12 +23,17 @@ public class Delete extends main {
             }
             break;
         }
-        showStore();
+        // when seller doesn't have any stores
+        if ( seller.getStores().size() == 0) {
+            System.out.println("This seller doesn't have any stores!");
+        } else {
+            showStore();
+        }
     }
     // show a list of stores and ask user to choose an index
     public void showStore() {
         Scanner scan = new Scanner(System.in);
-        
+
         int index = 0;
         boolean checkFormat1;
         ArrayList<Store> stores = seller.getStores();
@@ -43,7 +49,7 @@ public class Delete extends main {
                     System.out.println("Enter the index of the store you want to edit: ");
                     index = scan.nextInt();
                     scan.nextLine();
-                } catch (NumberFormatException e) {
+                } catch (InputMismatchException e) {
                     checkFormat1 = false;
                     System.out.println("Please enter the right format!");
                 }
@@ -57,7 +63,7 @@ public class Delete extends main {
         storeIndex = indexTemp - 1;
         Scanner scan = new Scanner(System.in);
         Store store = seller.getStores().get(storeIndex);
-        
+
         int indexProductTemp = 0;
         boolean checkFormat2;
         do {
@@ -67,14 +73,20 @@ public class Delete extends main {
                 int j = i + 1;
                 System.out.print(j + ". " + product.getName() + "\t\t");
                 nameCheck[i] = product.getName();
+                // explain message
+                if (nameCheck[i].equals("N/A")) {
+                    System.out.println("\n*N/A means the store doesn't contain any products*" +
+                                       "\n*if you remove N/A, you will remove the store itself*");
+                }
             }
+
             do {
                 checkFormat2 = true;
                 try {
-                    System.out.println("Enter the index of the store you want to edit: ");
+                    System.out.println("Enter the index of the product you want to delete: ");
                     indexProductTemp = scan.nextInt();
                     scan.nextLine();
-                } catch (NumberFormatException e) {
+                } catch (InputMismatchException e) {
                     checkFormat2 = false;
                     System.out.println("Please enter the right format!");
                 }
@@ -82,6 +94,23 @@ public class Delete extends main {
         } while (indexProductTemp <= 0 || indexProductTemp > store.getProducts().size());
 
         indexProduct = indexProductTemp - 1;
-        sellers.get(sellerIndex).getStores().get(storeIndex).deleteProduct(indexProduct);
+
+        if ( sellers.get(sellerIndex).getStores().get(storeIndex).getProducts().get(0).getName().equals("N/A") ) {
+            sellers.get(sellerIndex).getStores().remove(storeIndex);
+            System.out.println("Store removed!");
+        } else {
+            sellers.get(sellerIndex).getStores().get(storeIndex).deleteProduct(indexProduct);
+            System.out.println("Product deleted!");
+
+            // when the last product is deleted, add a dummy product named "N/A"
+            if (sellers.get(sellerIndex).getStores().get(storeIndex).getProducts().size() == 0) {
+                Product prodNull = new Product("N/A","N/A","N/A",0,0.0);
+                prodNull.setReviews(new ArrayList<>());
+                prodNull.setLimit(0);
+                sellers.get(sellerIndex).getStores().get(storeIndex).addProduct(prodNull);
+            }
+        }
+
     }
 }
+
