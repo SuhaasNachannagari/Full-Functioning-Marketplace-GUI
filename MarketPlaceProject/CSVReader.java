da95a1a0c18ea5d278d5479eea6815f7ec0e7509
suhaasnachannagari <mnachann@purdue.edu>
2023-11-29 17:38:20 -0500

    Reverted CSV file

--- MarketPlaceProject/CSVReader.java
+++ MarketPlaceProject/CSVReader.java
@@ -8,14 +8,6 @@ import java.io.IOException;
 *this class reades CSV file and also prints it
 */
 public class CSVReader {
-    public static void readCSV(String s) {
-        try (BufferedReader br = new BufferedReader(new FileReader(s))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     };
 }
