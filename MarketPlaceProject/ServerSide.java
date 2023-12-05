a7956345ba7617998531ace7f6bfb87af226e43d
Tri Vo <voquangtri2021@gmail.com>
2023-12-05 18:39:04 -0500

    Update ServerSide.java
    add code for Delete, Edit, and a small portion of Create classes

--- MarketPlaceProject/ServerSide.java
+++ MarketPlaceProject/ServerSide.java
@@ -55,7 +55,64 @@ class WorkFlow extends Thread {
                 String option = bfr.readLine();
                 switch (option) {
                     case "1 - Delete":
-                        MarketplaceServer.delete();
+                        String storeNames = MarketplaceServer.showStore();
+                        if (storeNames.equals("No Stores")) {
+                            //1d - ShowStore
+                            pw.write("No Stores\n");   pw.flush();
+                        } else {
+                            //1del - showStores
+                            pw.write(storeNames + "\n");   pw.flush();
+                            // 2del - storeChoice
+                            String storeChoice = bfr.readLine();
+                            String productNames = MarketplaceServer.showProducts(storeChoice);
+                            // 3del - showNames
+                            pw.write(productNames + "\n");   pw.flush();
+                            // 4del - productChoice
+                            String productChoice = bfr.readLine();
+                            String deleteResult = MarketplaceServer.deleteDeleteProduct(storeChoice, productChoice);
+                            // 5del - show delete result
+                            pw.write(deleteResult + "\n");   pw.flush();
+                        }
+                        break;
+                    case "2 - Edit":
+                        String storeNames2 = MarketplaceServer.showStore();
+                        if (storeNames2.equals("No Stores")) {
+                            //1edit - showStore
+                            pw.write("No Stores\n");    pw.flush();
+                        } else {
+                            //1edit - showStores
+                            pw.write(storeNames2 + "\n");  pw.flush();
+                            //2edit - storeChoice
+                            String storeChoice2 = bfr.readLine();
+                            //3edit - "no product" message
+                            String noProductMessage = MarketplaceServer.checkNoProductMessage(storeChoice2);
+                            pw.write(noProductMessage + "\n");  pw.flush();
+                            //4edit - show Products
+                            String productNames = MarketplaceServer.showProducts(storeChoice2);
+                            pw.write(productNames + "\n");  pw.flush();
+                            //5edit - productChoice
+                            String productChoice2 = bfr.readLine();
+                            //6edit - choice
+                            String choice = bfr.readLine();
+                            String valueChange = bfr.readLine();
+                            String editResult = MarketplaceServer.editProduct(storeChoice2, productChoice2,choice,
+                                    valueChange);
+                            //7edit- show Edit result
+                            pw.write(editResult + "\n");    pw.flush();
+                        }
+                    case "3 - Create":
+                        String storeNames3 = MarketplaceServer.showAllStore();
+                        //1cre - show store
+                        pw.write(storeNames3 + "\n");   pw.flush();
+                        //2cre - storeChoice
+                        String storeChoice3 = bfr.readLine();
+                        if (storeChoice3.equals("1. N/A") ||
+                              "Create new store".equals(storeChoice3.substring(storeChoice3.length() - 16))) {
+                            //3cre - Name to create new store
+                            String storeName = bfr.readLine();
+                        } else {
+                            // create new product
+                        }
                         break;
                 }
             }
